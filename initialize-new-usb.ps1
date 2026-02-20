8d0b92c7de4b8713852b5b7514c83f009dd452b7
ReevesA1 <adamrivard87@gmail.com>
2026-02-20 15:17:30 -0500

    s

--- initialize-new-usb.ps1
+++ initialize-new-usb.ps1
@@ -1,10 +1,7 @@
-$RepoUrl = "https://github.com/rocketpowerinc/rocketstick.git"
-
-Write-Host "Cloning RocketStick into current directory..."
-git clone $RepoUrl .
-
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "Clone complete."
-} else {
-    Write-Host "Clone failed."
+.{
+    git config --global --add safe.directory H:/
+    git init
+    git remote add origin https://github.com/rocketpowerinc/rocketstick.git 2>$null
+    git fetch origin
+    git checkout -f origin/main
 }
